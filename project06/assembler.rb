0a5323899c797569da958a5a7b52cb51513d0b57
Chad McKenna <cmckenna@mines.edu>
2011-02-27 12:22:08 -0700

    assembler.rb, parser.rb, and code.rb pass the first set of tests for non-symbol parser."

--- project06/assembler.rb
+++ project06/assembler.rb
@@ -1,6 +1,8 @@
 # author: Chad McKenna
 # HACK Assembler 
 
+require 'parser.rb'
+
 # Hack assembler class 
 class Assembler
   attr_reader :filename
@@ -8,7 +10,7 @@ class Assembler
   def initialize(filename)
     @filename = filename
     @input = File.open(filename, "r")
-    @output = File.new(filename.sub(/.asm/, "") + ".hack", "w")
+    @output = File.new(filename.sub(/.asm/, "") + ".mine" + ".hack", "w")
   end
   
   def write_to_output
@@ -16,7 +18,35 @@ class Assembler
   end
   
   def assemble
-    # To be implemented later
+    # This is the 1st pass parser
+    
+    parser = Parser.new(@input)
+    while(parser.has_more_commands?)
+      binary_command = nil.to_s
+      command_type = parser.command_type
+      if command_type.eql?('A_COMMAND')
+        # A_COMMAND Logic
+        binary_command += '0'
+        binary_command += '%015b' % parser.symbol.to_s
+      elsif command_type.eql?('L_COMMAND')
+        # L_COMMAND Logic
+        binary_command += '111'
+        binary_command += parser.symbol.to_s
+      elsif command_type.eql?('C_COMMAND')
+        # C_COMMAND Logic
+        binary_command += '111'
+        dest = parser.dest
+        comp = parser.comp
+        jump = parser.jump
+        binary_command += comp.to_s + dest.to_s + jump.to_s
+      else
+        binary_command = nil
+      end
+      if binary_command != nil
+        @output.puts binary_command
+      end
+      parser.advance
+    end
   end
   
   def close
@@ -25,10 +55,13 @@ class Assembler
   end
 end
 
+
+
+
 # Program control 
 
 # Make sure we're reading a .asm file
-if ARGV[0] =~ /.asm/
+if ARGV[0] =~ /.asm$/
   # Make sure they're only specifying one argument
   unless ARGV.length == 1
     puts "Yo bro, you don't have the right amount of arguments."
@@ -38,11 +71,13 @@ if ARGV[0] =~ /.asm/
   
   # Main control logic
   puts 'Compiling...'
-  run = Assembler.new(ARGV[0])
+  assembler = Assembler.new(ARGV[0])
 
-  run.write_to_output
+  assembler.assemble
   
-  run.close
+  assembler.close
+  
+  puts 'Done.'
 else 
   # Notification shown if not using .asm filetype
   puts "Hey man, you must specify a .asm filetype."
--- project06/parser.rb
+++ project06/parser.rb
@@ -1,56 +1,91 @@
+require 'code.rb'
+
 class Parser
-  attr_reader :read_file
-  
   # Reads a file into the parser
   def initialize(file)
-    @read_file = file
+    @read_file = Array.new
+    file.each_line { |line| @read_file.push(line) }
+    @current_line = 0
+  end
+  
+  def get_current_line
+    @read_file.at(@current_line)
   end
   
   # This functions looks to see if there are extra commands 
   # in the input. Returns boolean.
   def has_more_commands?
-    
+    @current_line != (@read_file.length)
   end
 
   # This function reads the next line/command from the input 
   # file and sets it as our current working command.
   # Only called if has_more_commands? is true.
   def advance
-    
+    if has_more_commands?
+      @current_line += 1
+      if @read_file.at(@current_line) =~ /\/\//
+        advance
+      end
+    end
   end
   
   # Returns teh type of the current command.
   # Return is either A_COMMAND, C_COMMAND, or L_COMMAND
   def command_type
-    
+    if @read_file.at(@current_line) =~ /^@/
+      'A_COMMAND'
+    elsif @read_file.at(@current_line) =~ /\(|\)/
+      'L_COMMAND'
+    elsif @read_file.at(@current_line) =~ /(=|;)/
+      'C_COMMAND'
+    else
+      nil
+    end
   end
   
   # Returns the symbol or decimal Xxx of the current command.
   # @Xxx or (Xxx). Should be called only when command_type is
   # A_COMMAND or L_COMMAND
   def symbol
-    
+    if command_type.eql?('A_COMMAND')
+      @read_file.at(@current_line).gsub('@', '').strip
+    elsif command_type.eql?('L_COMMAND')
+      @read_file.at(@current_line).gsub('(', '').gsub(')', '').strip
+    end
   end
   
   # Returns the dest mnemonic in the current C-command (8 
   # possibilities). Should be called only when command_type
   # is C_COMMAND. Returns a string.
   def dest
-    
+    if @read_file.at(@current_line) =~ /;/
+      Code.dest('null')
+    else
+      Code.dest(@read_file.at(@current_line).split('=').at(0).strip)
+    end
   end
   
   # Returns the comp mnemonic in the current C-command (28
   # possibilities). Should be called only when command_type
   # is C_COMMAND. Returns a string.
   def comp
-    
+    if @read_file.at(@current_line) =~ /;/
+      Code.comp(@read_file.at(@current_line).split(';').at(0).strip)
+    else
+      Code.comp(@read_file.at(@current_line).split('=').at(1).strip)
+    end
   end
   
   # Returns the jump mnemonic in the current C-command (8 
   # possibilities). Should be called only when command_type
   # is C_COMMAND.
   def jump
-    
+    if @read_file.at(@current_line) =~ /;/
+      Code.jump(@read_file.at(@current_line).split(';').at(1).strip)
+    else
+      Code.jump('null')
+    end
   end
   
 end
