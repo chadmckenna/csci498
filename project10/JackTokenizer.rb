9daefc13b82726cce2cd61afee0ded935e138280
Chad McKenna <cmckenna@mines.edu>
2011-04-11 13:28:08 -0600

    Changed spelling error in JackTokenizer.rb

--- project10/JackTokenizer.rb
+++ project10/JackTokenizer.rb
@@ -1,6 +1,6 @@
 class JackTokenizer
 
-  KEYWORDS = ["CLASS", "CONSTURCTOR", "FUNCTION", "METHOD", "FIELD", "STATIC", "VAR", "INT", "CHAR", "BOOLEAN", "VOID", "TRUE", "FALSE", "NULL", "THIS",
+  KEYWORDS = ["CLASS", "CONSTRUCTOR", "FUNCTION", "METHOD", "FIELD", "STATIC", "VAR", "INT", "CHAR", "BOOLEAN", "VOID", "TRUE", "FALSE", "NULL", "THIS",
     "LET", "DO", "IF", "ELSE", "WHILE", "RETURN"]
   SYMBOLS = ["{", "}", "(", ")", "[", "]", ".", ",", ";", "+", "-", "*", "/", "&", "|", "<", ">", "=", "~"]
 	
