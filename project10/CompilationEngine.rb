602226ba3d14e16d00f98bcb7a76637b363c8de8
Chad McKenna <cmckenna@mines.edu>
2011-04-14 20:30:15 -0600

    Big changes to CompEngine and JackAnalyzer

--- project10/CompilationEngine.rb
+++ project10/CompilationEngine.rb
@@ -1,12 +1,67 @@
 class CompilationEngine
-  def initialize(output)
-    @output = output
-    @output.write("<tokens>\n")
+  def initialize(tokenizer)
+    @tokenizer = tokenizer
+    @
   end
   
-  def close
-    @output.write("</tokens>\n")
-    @output.close
+  def compile
+    while (@tokenizer.has_more_tokens?)
+      compile_next_token
+    end
+  end
+  
+  def compile_next_token
+    @tokenizer.advance
+    token_type = @tokenizer.token_type
+    if token_type.eql?('KEYWORD')
+      key_word = @tokenizer.key_word
+      #@output.write("<keyword> " + key_word.downcase + " </keyword>\n")
+      if key_word.eql?('CLASS')
+        compile_class
+      elsif key_word.eql?('METHOD') or key_word.eql?('FUNCTION') or key_word.eql?('CONSTRUCTOR')
+        compile_subroutine
+      elsif (key_word.eql?('INT') or key_word.eql?('BOOLEAN') or key_word.eql?('BOOLEAN') or key_word.eql?('CHAR') or key_word.eql?('VOID') 
+        or key_word.eql?('TRUE') or key_word.eql?('FALSE') or key_word.eql?('NULL') or key_word.eql?('THIS'))
+        @output.write("<keyword> " + key_word.downcase + " </keyword>\n")
+      elsif key_word.eql?('VAR')
+        compile_var_dec
+      elsif key_word.eql?('STATIC') or key_word.eql?('FIELD')
+        compile_class_var_dec
+      elsif key_word.eql?('LET')
+        compile_let
+      elsif key_word.eql?('DO')
+        compile_do
+      elsif key_word.eql?('IF') or key_word.eql?('ELSE')
+        compile_if
+      elsif key_word.eql?('WHILE')
+        compile_while
+      elsif key_word.eql?('RETURN')
+        compile_return
+      else
+        puts 'Invalid keyword call.'
+      end
+    elsif token_type.eql?('SYMBOL')
+      symbol = @tokenizer.symbol
+      if (symbol.eql?('<'))
+        symbol = '&lt;'
+      elsif (symbol.eql?('>'))
+        symbol = '&gt;'
+      end
+      @output.write("<symbol> " + symbol + " </symbol>\n")
+    elsif token_type.eql?('IDENTIFIER')
+      identifier = @tokenizer.identifier
+      @output.write("<identifier> " + identifier + " </identifier>\n")
+    elsif token_type.eql?('INT_CONST')
+      int_val = @tokenizer.int_val
+      @output.write("<integerConstant> " + int_val + " </integerConstant>\n")
+    elsif token_type.eql?('STRING_CONST')
+      string_val = @tokenizer.string_val
+      string_val.gsub!(/&/, "&amp;")
+      string_val.gsub!(/\"/, "")
+      @output.write("<stringConstant> " + string_val + " </stringConstant>\n")
+    else
+      puts 'Invalid token call.'
+    end
   end
   
   def compile_class
@@ -14,54 +69,42 @@ class CompilationEngine
   end
   
   def compile_class_var_dec
-    @output.write('Class')
+    
   end
   
   def compile_subroutine
-    @output.write('Class')
   end
   
   def compile_parameter_list
-    @output.write('Class')
   end
   
   def compile_var_dec
-    @output.write('Class')
   end
   
   def compile_statements
-    @output.write('Class')
   end
   
   def compile_do
-    @output.write('Class')
   end
   
   def compile_let
-    @output.write('Class')
   end
   
   def compile_while
-    @output.write('Class')
   end
   
   def compile_return
-    @output.write('Class')
   end
   
   def compile_if
-    @output.write('Class')
   end
   
   def compile_expression
-    @output.write('Class')
   end
   
   def compile_term
-    @output.write('Class')
   end
   
   def compile_expression_list
-    @output.write('Class')
   end
 end
--- project10/JackAnalyzer.rb
+++ project10/JackAnalyzer.rb
@@ -16,89 +16,12 @@ class JackAnalyzer
   def tokenize
     # Tokenize code goes here
     tokenizer = JackTokenizer.new(@input)
+    comp_engine = CompilationEngine.new(tokenizer)
     #compiler = CompilationEngine.new(@output)
-    @output.write("<tokens>\n")
-    while (tokenizer.has_more_tokens?)
-      tokenizer.advance
-      token_type = tokenizer.token_type
-      if token_type.eql?('KEYWORD')
-        key_word = tokenizer.key_word
-        @output.write("<keyword> " + key_word.downcase + " </keyword>\n")
-        #if key_word.eql?('CLASS')
-        #  compiler.compile_class
-        #elsif key_word.eql?('METHOD')
-          # Do something in CompEngine
-        #elsif key_word.eql?('FUNCTION')
-          # Do something in CompEngine
-        #elsif key_word.eql?('CONSTRUCTOR')  
-          # Do something in CompEngine
-        #elsif key_word.eql?('INT')
-          # Do something in CompEngine
-        #elsif key_word.eql?('BOOLEAN')
-          # Do something in CompEngine
-        #elsif key_word.eql?('CHAR')
-          # Do something in CompEngine
-        #elsif key_word.eql?('VOID')
-          # Do something in CompEngine
-        #elsif key_word.eql?('VAR')
-          # Do something in CompEngine
-        #elsif key_word.eql?('STATIC')
-          # Do something in CompEngine
-        #elsif key_word.eql?('FIELD')
-          # Do something in CompEngine
-        #elsif key_word.eql?('LET')
-          # Do something in CompEngine
-        #elsif key_word.eql?('DO')
-          # Do something in CompEngine
-        #elsif key_word.eql?('IF')
-          # Do something in CompEngine
-        #elsif key_word.eql?('ELSE')
-          # Do something in CompEngine
-        #elsif key_word.eql?('WHILE')
-          # Do something in CompEngine
-        #elsif key_word.eql?('RETURN')
-          # Do something in CompEngine
-        #elsif key_word.eql?('TRUE')
-          # Do something in CompEngine
-        #elsif key_word.eql?('FALSE')
-          # Do something in CompEngine
-        #elsif key_word.eql?('NULL')
-          # Do something in CompEngine
-        #elsif key_word.eql?('THIS')
-          # Do something in CompEngine
-        #else
-        #  puts 'Invalid keyword call.'
-        #end
-      elsif token_type.eql?('SYMBOL')
-        symbol = tokenizer.symbol
-        if (symbol.eql?('<'))
-          symbol = '&lt;'
-        elsif (symbol.eql?('>'))
-          symbol = '&gt;'
-        end
-        @output.write("<symbol> " + symbol + " </symbol>\n")
-        # Do something in CompEngine
-      elsif token_type.eql?('IDENTIFIER')
-        identifier = tokenizer.identifier
-        @output.write("<identifier> " + identifier + " </identifier>\n")
-        # Do something in CompEngine
-      elsif token_type.eql?('INT_CONST')
-        int_val = tokenizer.int_val
-        @output.write("<integerConstant> " + int_val + " </integerConstant>\n")
-        # Do something in CompEngine
-      elsif token_type.eql?('STRING_CONST')
-        string_val = tokenizer.string_val
-        string_val.gsub!(/&/, "&amp;")
-        string_val.gsub!(/\"/, "")
-        @output.write("<stringConstant> " + string_val + " </stringConstant>\n")
-        # Do something in CompEngine
-      else
-        puts 'Invalid token call.'
-      end
-      #tokenizer.advance
-    end
-    #compiler.close
-    @output.write("</tokens>\n")
+    comp_engine.compile
+    
+    @input.close
+    @output.close
   end
   
 end
