67fd64f8f555c7156193851a52e59cafbe810049
Chad McKenna <cmckenna@mines.edu>
2011-04-14 22:49:47 -0600

    Comp engine

--- project10/CompilationEngine.rb
+++ project10/CompilationEngine.rb
@@ -97,8 +97,11 @@ class CompilationEngine
 	
 	while (@tokenizer.key_word.eql?("STATIC") or @tokenizer.key_word.eql?("FIELD"))
 		compile_class_var_dec
+	end
+	
 	while (@tokenizer.key_word.eql?("CONSTRUCTOR") or @tokenizer.key_word.eql?("FUNCTION") or @tokenizer.key_word.eql?("METHOD"))
 		compile_subroutine
+	end
 	
 	if !(@tokenizer.symbol.eql("}"))
 		puts"expected }"
@@ -129,6 +132,9 @@ class CompilationEngine
   end
   
   def compile_let
+    @output.write("<letStatement>")
+    
+    @output.write("</letStatement>")
   end
   
   def compile_while
