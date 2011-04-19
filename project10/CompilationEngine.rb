a9f1c343c9bfaa97fafce21cdad4171d3df8eae9
tmesquita <tmesquit@mines.edu>
2011-04-18 22:48:22 -0600

    expression parsing is close to done

--- project10/CompilationEngine.rb
+++ project10/CompilationEngine.rb
@@ -12,56 +12,6 @@ class CompilationEngine
   	if(@tokenizer.has_more_tokens?)
     	@tokenizer.advance
 	end
-#    token_type = @tokenizer.token_type
-#  if token_type.eql?('KEYWORD')
-#    key_word = @tokenizer.key_word
-#    #@output.write("<keyword> " + key_word.downcase + " </keyword>\n")
-#    if key_word.eql?('CLASS')
-#      compile_class
-#    elsif key_word.eql?('METHOD') or key_word.eql?('FUNCTION') or key_word.eql?('CONSTRUCTOR')
-#      compile_subroutine
-#    elsif (key_word.eql?('INT') or key_word.eql?('BOOLEAN') or key_word.eql?('BOOLEAN') or #y_word.eql?('CHAR') or key_word.eql?('VOID') 
-#      or key_word.eql?('TRUE') or key_word.eql?('FALSE') or key_word.eql?('NULL') or #y_word.eql?('THIS'))
-#      @output.write("<keyword> " + key_word.downcase + " </keyword>\n")
-#    elsif key_word.eql?('VAR')
-#      compile_var_dec
-#    elsif key_word.eql?('STATIC') or key_word.eql?('FIELD')
-#      compile_class_var_dec
-#    elsif key_word.eql?('LET')
-#      compile_let
-#    elsif key_word.eql?('DO')
-#      compile_do
-#    elsif key_word.eql?('IF') or key_word.eql?('ELSE')
-#      compile_if
-#    elsif key_word.eql?('WHILE')
-#      compile_while
-#    elsif key_word.eql?('RETURN')
-#      compile_return
-#    else
-#      puts 'Invalid keyword call.'
-#    end
-#  elsif token_type.eql?('SYMBOL')
-#    symbol = @tokenizer.symbol
-#    if (symbol.eql?('<'))
-#      symbol = '&lt;'
-#    elsif (symbol.eql?('>'))
-#      symbol = '&gt;'
-#    end
-#    @output.write("<symbol> " + symbol + " </symbol>\n")
-#  elsif token_type.eql?('IDENTIFIER')
-#    identifier = @tokenizer.identifier
-#    @output.write("<identifier> " + identifier + " </identifier>\n")
-#  elsif token_type.eql?('INT_CONST')
-#    int_val = @tokenizer.int_val
-#    @output.write("<integerConstant> " + int_val + " </integerConstant>\n")
-#  elsif token_type.eql?('STRING_CONST')
-#    string_val = @tokenizer.string_val
-#    string_val.gsub!(/&/, "&amp;")
-#    string_val.gsub!(/\"/, "")
-#    @output.write("<stringConstant> " + string_val + " </stringConstant>\n")
-#  else
-#    puts 'Invalid token call.'
-#  end
   end
   
   def compile_class
@@ -428,9 +378,11 @@ class CompilationEngine
   end
   
   def compile_term
-    @output.write("<term>\n")
+    @advance = false
+  	@output.write("<term>\n")
     if @tokenizer.token_type.eql?("STRING_CONST") or @tokenizer.token_type.eql?("INT_CONST") or @tokenizer.token_type.eql?("KEYWORD")
-    	@output.write(@tokenizer.print_token
+    	@output.write(@tokenizer.print_token)
+    	@advance = true
     elsif @tokenizer.symbol.eql?("(")
     	#(
     	@output.write(@tokenizer.print_token)
@@ -438,11 +390,51 @@ class CompilationEngine
     	compile_expression
     	#)
     	@output.write(@tokenizer.print_token)
+    	@advance = true
     elsif @tokenizer.symbol.eql?("~") or @tokenizer.symbol.eql?("-")
     	#~ or -
     	@output.write(@tokenizer.print_token)
     	compile_next_token
     	compile_term
+	elsif @tokenizer.token_type.eql?("IDENTIFIER")
+    	@output.write(@tokenizer.print_token)
+    	compile_next_token
+    	if @tokenizer.symbol.eql?("[")
+    		#[
+    		@output.write(@tokenizer.print_token)
+    		compile_next_token
+    		compile_expression
+    		#]
+    		@output.write(@tokenizer.print_token)
+    		@advance = true
+        elsif @tokenizer.symbol.eql?("(")
+        	#(
+        	@output.write(@tokenizer.print_token)
+    		compile_next_token
+			compile_expression_list
+			compile_next_token
+			#)
+			@output.write(@tokenizer.print_token)
+			@advance = true
+		 elsif @tokenizer.symbol.eql?(".")
+	    	#.
+	    	@output.write(@tokenizer.print_token)
+	    	compile_next_token
+	    	#identifier
+	    	@output.write(@tokenizer.print_token)
+	    	compile_next_token
+	    	#(
+	    	@output.write(@tokenizer.print_token)
+	    	compile_next_token
+	    	compile_expression_list
+	    	#)
+	    	@output.write(@tokenizer.print_token)    	
+	    	@advance = true
+    	end
+    end
+    if @advance
+    	compile_next_token
+    end
     #more implementation needed
     #compile_next_token
     @output.write("</term>\n")
@@ -450,14 +442,11 @@ class CompilationEngine
   
   def compile_expression_list
     @output.write("<expressionList>\n")
-    while !@tokenizer.symbol.eql?(')')
-      compile_expression
-      compile_next_token
-      # Symbol ','
-      if @tokenizer.symbol.eql?(",")
-        @output.write(@tokenizer.print_token)
-        compile_next_token
-      end
+	compile_expression
+    while @tokenizer.symbol.eql?(",")
+    	@output.write(@tokenizer.print_token)
+		compile_next_token
+    	compile_expression
     end
     @output.write("</expressionList>\n")
   end
