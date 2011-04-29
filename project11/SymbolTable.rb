c53a19145fabf27014a0a7b4bea4903a06949fbc
tmesquita <tmesquit@mines.edu>
2011-04-28 22:05:33 -0600

    added symbolTable and VMwriter skeletons

---
+++ project11/SymbolTable.rb
@@ -0,0 +1,29 @@
+class SymbolTable
+	def initialize
+		
+	end
+	
+	def start_subroutine
+		
+	end
+	
+	def define(name, type, kind)
+		
+	end
+	
+	def var_count(kind)
+		
+	end
+	
+	def kind_of(name)
+		
+	end
+	
+	def type_of(name)
+		
+	end
+	
+	def index_of(name)
+		
+	end
+end
---
+++ project11/VMWriter.rb
@@ -0,0 +1,45 @@
+class VMWriter
+	def initialize(output)
+		
+	end
+	
+	def write_push(segment, index)
+		
+	end
+	
+	def write_pop(segment, index)
+	
+	end
+	
+	def write_arithmetic(command)
+		
+	end
+	
+	def write_label(label)
+		
+	end
+	
+	def write_goto(label)
+		
+	end
+	
+	def write_if(label)
+		
+	end
+	
+	def write_call(name, nArgs)
+		
+	end
+	
+	def write_function(name, nLocals)
+		
+	end
+	
+	def write_return
+		
+	end
+	
+	def close
+		
+	end
+end
