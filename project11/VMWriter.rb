7360dbc7d7232785b4032a863912b736f90bd8ed
tmesquita <tmesquit@mines.edu>
2011-04-30 13:59:48 -0600

    implemented VMWriter.rb

--- project11/VMWriter.rb
+++ project11/VMWriter.rb
@@ -1,45 +1,42 @@
 class VMWriter
 	def initialize(output)
-		
+		@output = output
 	end
 	
 	def write_push(segment, index)
-		
+		@ouput.write("push" + segment + " " + index + "\n"))
 	end
 	
 	def write_pop(segment, index)
-	
+		@output.write("pop" + segment + " " + index + "\n")
 	end
 	
 	def write_arithmetic(command)
-		
+		@output.write(command + "\n")
 	end
 	
 	def write_label(label)
-		
+		@output.write("label" + label + "\n")
 	end
 	
 	def write_goto(label)
-		
+		@output.write("goto" + label + "\n")
 	end
 	
 	def write_if(label)
-		
+		@output.write("if-goto" + label + "\n")
 	end
 	
 	def write_call(name, nArgs)
-		
+		@output.write("call" + name + " " + nArgs + "\n")
 	end
 	
 	def write_function(name, nLocals)
-		
+		@output.write("function" + name + " " + nLocals + "\n")		
 	end
 	
 	def write_return
-		
+		@output.write("return\n")
 	end
-	
-	def close
-		
-	end
+
 end
