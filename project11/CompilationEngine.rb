fe382f5a596cce2df636af066dde0bd5b2a4732a
tmesquita <tmesquit@mines.edu>
2011-05-02 18:50:01 -0600

    passing vm output file to compilation engine

--- project11/CompilationEngine.rb
+++ project11/CompilationEngine.rb
@@ -1,7 +1,8 @@
 class CompilationEngine
-  def initialize(tokenizer, output)
+  def initialize(tokenizer, vmWriter, output)
     @tokenizer = tokenizer
     @output = output
+    @vm_writer = vmWriter
   end
   
   def compile
@@ -12,56 +13,6 @@ class CompilationEngine
   	if(@tokenizer.has_more_tokens?)
     	@tokenizer.advance
 	end
-#    token_type = @tokenizer.token_type
-#  if token_type.eql?('KEYWORD')
-#    key_word = @tokenizer.key_word
-#    #@output.write("<keyword> " + key_word.downcase + " </keyword>\n")
-#    if key_word.eql?('CLASS')
-#      compile_class
-#    elsif key_word.eql?('METHOD') or key_word.eql?('FUNCTION') or key_word.eql?('CONSTRUCTOR')
-#      compile_subroutine
-#    elsif (key_word.eql?('INT') or key_word.eql?('BOOLEAN') or key_word.eql?('BOOLEAN') or #y_word.eql?('CHAR') or key_word.eql?('VOID') 
-#      or key_word.eql?('TRUE') or key_word.eql?('FALSE') or key_word.eql?('NULL') or #y_word.eql?('THIS'))
-#      @output.write("<keyword> " + key_word.downcase + " </keyword>\n")
-#    elsif key_word.eql?('VAR')
-#      compile_var_dec
-#    elsif key_word.eql?('STATIC') or key_word.eql?('FIELD')
-#      compile_class_var_dec
-#    elsif key_word.eql?('LET')
-#      compile_let
-#    elsif key_word.eql?('DO')
-#      compile_do
-#    elsif key_word.eql?('IF') or key_word.eql?('ELSE')
-#      compile_if
-#    elsif key_word.eql?('WHILE')
-#      compile_while
-#    elsif key_word.eql?('RETURN')
-#      compile_return
-#    else
-#      puts 'Invalid keyword call.'
-#    end
-#  elsif token_type.eql?('SYMBOL')
-#    symbol = @tokenizer.symbol
-#    if (symbol.eql?('<'))
-#      symbol = '&lt;'
-#    elsif (symbol.eql?('>'))
-#      symbol = '&gt;'
-#    end
-#    @output.write("<symbol> " + symbol + " </symbol>\n")
-#  elsif token_type.eql?('IDENTIFIER')
-#    identifier = @tokenizer.identifier
-#    @output.write("<identifier> " + identifier + " </identifier>\n")
-#  elsif token_type.eql?('INT_CONST')
-#    int_val = @tokenizer.int_val
-#    @output.write("<integerConstant> " + int_val + " </integerConstant>\n")
-#  elsif token_type.eql?('STRING_CONST')
-#    string_val = @tokenizer.string_val
-#    string_val.gsub!(/&/, "&amp;")
-#    string_val.gsub!(/\"/, "")
-#    @output.write("<stringConstant> " + string_val + " </stringConstant>\n")
-#  else
-#    puts 'Invalid token call.'
-#  end
   end
   
   def compile_class
@@ -84,12 +35,7 @@ class CompilationEngine
   	@output.write(@tokenizer.print_token)
 
   	compile_next_token
-
-  	#if !@tokenizer.symbol.token_type.eql?("{")
-  	#	puts"expected {"
-  	#	return
-  	#end
-	  if !@tokenizer.symbol.eql?("{")
+	if !@tokenizer.symbol.eql?("{")
   		puts"expected {"
   		return
   	end
